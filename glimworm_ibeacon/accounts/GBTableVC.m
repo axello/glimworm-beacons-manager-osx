7a962a7a2a4d3fe7dceafbaee7c1cf1735bc7832
Axel Roest <github@phlux.us>
2015-10-23 14:39:02 +0200

    Added class, disabled actions, small tweaks storyboard

---
+++ glimworm_ibeacon/accounts/GBTableVC.m
@@ -0,0 +1,22 @@
+//
+//  GBTableVC.m
+//  glimworm_ibeacon
+//
+//  Created by Axel Roest on 23/10/15.
+//  Copyright © 2015 Jonathan Carter. All rights reserved.
+//
+
+#import "GBTableVC.h"
+
+@interface GBTableVC ()
+
+@end
+
+@implementation GBTableVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
